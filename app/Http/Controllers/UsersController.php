64faf140f7bd3bed9b1ad5c3fe07ed266617e66f
yoshiki.nakano <nakano.yoshiki@a-tm.co.jp>
2018-05-15 07:27:10 +0000

    follow, unfollow

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\User; // 追加
+use App\User;
+use App\Micropost;
 
 class UsersController extends Controller
 {
@@ -24,6 +25,36 @@ class UsersController extends Controller
         return view('users.show', [
             'user' => $user,
         ]);
+    }
+    
+    public function followings($id)
+    {
+        $user = User::find($id);
+        $followings = $user->followings()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'users' => $followings,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.followings', $data);
+    }
+
+    public function followers($id)
+    {
+        $user = User::find($id);
+        $followers = $user->followers()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'users' => $followers,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.followers', $data);
     }
     
     
