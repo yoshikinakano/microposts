19ce976e618b09b0493ef004a2af3fea3813d500
yoshiki.nakano <nakano.yoshiki@a-tm.co.jp>
2018-05-15 07:30:49 +0000

    timeline

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -14,23 +14,20 @@ class MicropostsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+  
     public function index()
     {
-
         $data = [];
         if (\Auth::check()) {
             $user = \Auth::user();
-            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
 
             $data = [
                 'user' => $user,
                 'microposts' => $microposts,
             ];
-            $data += $this->counts($user);
-            return view('users.show', $data);
-        }else {
-            return view('welcome');
         }
+        return view('welcome', $data);
     }
     
     public function store(Request $request)
